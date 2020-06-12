ee8925f5684f03af6486210965de4b7205172d93
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-06-12 08:03:13 -0300

    Stable version with sources.

--- application/playground/build.gradle.kts
+++ application/playground/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 dependencies {
 	implementation(kotlin("stdlib-jdk8"))
 
-	implementation("com.daniloaraujosilva.mathemagika:jvm:0.0.1")
+	implementation("com.daniloaraujosilva.mathemagika:library:0.0.1")
 
 	implementation(
 		mapOf("name" to "JLink")

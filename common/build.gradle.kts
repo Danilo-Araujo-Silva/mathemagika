a8ee0cac8391d8a2f7017f64ab0f088dee1dcde9
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-06-13 18:49:27 -0300

    Disabling unused targets and source sets.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -6,33 +6,33 @@ plugins {
 }
 
 kotlin {
-	androidNativeArm32 {
-	}
-
-	androidNativeArm64 {
-	}
-
-	iosArm32 {
-		binaries {
-			framework {}
-		}
-	}
-
-	iosArm64 {
-		binaries {
-			framework {}
-		}
-	}
-
-	iosX64 {
-		binaries {
-			framework {}
-		}
-	}
+//	androidNativeArm32 {
+//	}
+//
+//	androidNativeArm64 {
+//	}
+//
+//	iosArm32 {
+//		binaries {
+//			framework {}
+//		}
+//	}
+//
+//	iosArm64 {
+//		binaries {
+//			framework {}
+//		}
+//	}
+//
+//	iosX64 {
+//		binaries {
+//			framework {}
+//		}
+//	}
 
 	js {
-		browser {
-		}
+//		browser {
+//		}
 
 		nodejs {
 		}
@@ -41,26 +41,26 @@ kotlin {
 	jvm {
 	}
 
-	linuxArm32Hfp {
-	}
-
-	linuxMips32 {
-	}
-
-	linuxMipsel32 {
-	}
-
-	linuxX64 {
-	}
+//	linuxArm32Hfp {
+//	}
+//
+//	linuxMips32 {
+//	}
+//
+//	linuxMipsel32 {
+//	}
+//
+//	linuxX64 {
+//	}
 
 	macosX64 {
 	}
 
-	mingwX64 {
-	}
-
-	wasm32 {
-	}
+//	mingwX64 {
+//	}
+//
+//	wasm32 {
+//	}
 
 	sourceSets {
 		val commonMain by getting {
@@ -91,60 +91,60 @@ kotlin {
 			dependsOn(commonTest)
 		}
 
-		val androidNativeArm32Main by getting {
-			configureSourceSet(this, "android_native_arm32")
-
-			dependsOn(nativeMain)
-		}
-		val androidNativeArm32Test by getting {
-			configureSourceSet(this, "android_native_arm32")
-
-			dependsOn(nativeTest)
-		}
-
-		val androidNativeArm64Main by getting {
-			configureSourceSet(this, "android_native_arm64")
-
-			dependsOn(nativeMain)
-		}
-		val androidNativeArm64Test by getting {
-			configureSourceSet(this, "android_native_arm64")
-
-			dependsOn(nativeTest)
-		}
-
-		val iosArm32Main by getting {
-			configureSourceSet(this, "ios_arm32")
-
-			dependsOn(nativeMain)
-		}
-		val iosArm32Test by getting {
-			configureSourceSet(this, "ios_arm32")
-
-			dependsOn(nativeTest)
-		}
-
-		val iosArm64Main by getting {
-			configureSourceSet(this, "ios_arm64")
-
-			dependsOn(nativeMain)
-		}
-		val iosArm64Test by getting {
-			configureSourceSet(this, "ios_arm64")
-
-			dependsOn(nativeTest)
-		}
-
-		val iosX64Main by getting {
-			configureSourceSet(this, "ios_x64")
-
-			dependsOn(nativeMain)
-		}
-		val iosX64Test by getting {
-			configureSourceSet(this, "ios_x64")
-
-			dependsOn(nativeTest)
-		}
+//		val androidNativeArm32Main by getting {
+//			configureSourceSet(this, "android_native_arm32")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val androidNativeArm32Test by getting {
+//			configureSourceSet(this, "android_native_arm32")
+//
+//			dependsOn(nativeTest)
+//		}
+//
+//		val androidNativeArm64Main by getting {
+//			configureSourceSet(this, "android_native_arm64")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val androidNativeArm64Test by getting {
+//			configureSourceSet(this, "android_native_arm64")
+//
+//			dependsOn(nativeTest)
+//		}
+//
+//		val iosArm32Main by getting {
+//			configureSourceSet(this, "ios_arm32")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val iosArm32Test by getting {
+//			configureSourceSet(this, "ios_arm32")
+//
+//			dependsOn(nativeTest)
+//		}
+//
+//		val iosArm64Main by getting {
+//			configureSourceSet(this, "ios_arm64")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val iosArm64Test by getting {
+//			configureSourceSet(this, "ios_arm64")
+//
+//			dependsOn(nativeTest)
+//		}
+//
+//		val iosX64Main by getting {
+//			configureSourceSet(this, "ios_x64")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val iosX64Test by getting {
+//			configureSourceSet(this, "ios_x64")
+//
+//			dependsOn(nativeTest)
+//		}
 
 		val jsMain by getting {
 			configureSourceSet(this, "js")
@@ -177,49 +177,49 @@ kotlin {
 			}
 		}
 
-		val linuxArm32HfpMain by getting {
-			configureSourceSet(this, "linux_arm32_hfp")
-
-			dependsOn(nativeMain)
-		}
-		val linuxArm32HfpTest by getting {
-			configureSourceSet(this, "linux_arm32_hfp")
-
-			dependsOn(nativeTest)
-		}
-
-		val linuxMips32Main by getting {
-			configureSourceSet(this, "linux_mips32")
-
-			dependsOn(nativeMain)
-		}
-		val linuxMips32Test by getting {
-			configureSourceSet(this, "linux_mips32")
-
-			dependsOn(nativeTest)
-		}
-
-		val linuxMipsel32Main by getting {
-			configureSourceSet(this, "linux_mipsel32")
-
-			dependsOn(nativeMain)
-		}
-		val linuxMipsel32Test by getting {
-			configureSourceSet(this, "linux_mipsel32")
-
-			dependsOn(nativeTest)
-		}
-
-		val linuxX64Main by getting {
-			configureSourceSet(this, "linux_x64")
-
-			dependsOn(nativeMain)
-		}
-		val linuxX64Test by getting {
-			configureSourceSet(this, "linux_x64")
-
-			dependsOn(nativeTest)
-		}
+//		val linuxArm32HfpMain by getting {
+//			configureSourceSet(this, "linux_arm32_hfp")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val linuxArm32HfpTest by getting {
+//			configureSourceSet(this, "linux_arm32_hfp")
+//
+//			dependsOn(nativeTest)
+//		}
+//
+//		val linuxMips32Main by getting {
+//			configureSourceSet(this, "linux_mips32")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val linuxMips32Test by getting {
+//			configureSourceSet(this, "linux_mips32")
+//
+//			dependsOn(nativeTest)
+//		}
+//
+//		val linuxMipsel32Main by getting {
+//			configureSourceSet(this, "linux_mipsel32")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val linuxMipsel32Test by getting {
+//			configureSourceSet(this, "linux_mipsel32")
+//
+//			dependsOn(nativeTest)
+//		}
+//
+//		val linuxX64Main by getting {
+//			configureSourceSet(this, "linux_x64")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val linuxX64Test by getting {
+//			configureSourceSet(this, "linux_x64")
+//
+//			dependsOn(nativeTest)
+//		}
 
 		val macosX64Main by getting {
 			configureSourceSet(this, "macos_x64")
@@ -232,27 +232,27 @@ kotlin {
 			dependsOn(nativeTest)
 		}
 
-		val mingwX64Main by getting {
-			configureSourceSet(this, "mingw_x64")
-
-			dependsOn(nativeMain)
-		}
-		val mingwX64Test by getting {
-			configureSourceSet(this, "mingw_x64")
-
-			dependsOn(nativeTest)
-		}
-
-		val wasm32Main by getting {
-			configureSourceSet(this, "wasm32")
-
-			dependsOn(nativeMain)
-		}
-		val wasm32Test by getting {
-			configureSourceSet(this, "wasm32")
-
-			dependsOn(nativeTest)
-		}
+//		val mingwX64Main by getting {
+//			configureSourceSet(this, "mingw_x64")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val mingwX64Test by getting {
+//			configureSourceSet(this, "mingw_x64")
+//
+//			dependsOn(nativeTest)
+//		}
+//
+//		val wasm32Main by getting {
+//			configureSourceSet(this, "wasm32")
+//
+//			dependsOn(nativeMain)
+//		}
+//		val wasm32Test by getting {
+//			configureSourceSet(this, "wasm32")
+//
+//			dependsOn(nativeTest)
+//		}
 	}
 }
 

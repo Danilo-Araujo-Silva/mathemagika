5fd07487326322635fb256b34de0c5f30eeb4985
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-06-13 18:45:14 -0300

    Successful build enabling all targets and source sets for testing.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -6,33 +6,33 @@ plugins {
 }
 
 kotlin {
-//	androidNativeArm32 {
-//	}
-//
-//	androidNativeArm64 {
-//	}
-//
-//	iosArm32 {
-//		binaries {
-//			framework {}
-//		}
-//	}
-//
-//	iosArm64 {
-//		binaries {
-//			framework {}
-//		}
-//	}
-//
-//	iosX64 {
-//		binaries {
-//			framework {}
-//		}
-//	}
+	androidNativeArm32 {
+	}
+
+	androidNativeArm64 {
+	}
+
+	iosArm32 {
+		binaries {
+			framework {}
+		}
+	}
+
+	iosArm64 {
+		binaries {
+			framework {}
+		}
+	}
+
+	iosX64 {
+		binaries {
+			framework {}
+		}
+	}
 
 	js {
-//		browser {
-//		}
+		browser {
+		}
 
 		nodejs {
 		}
@@ -41,30 +41,30 @@ kotlin {
 	jvm {
 	}
 
-//	linuxArm32Hfp {
-//	}
-//
-//	linuxMips32 {
-//	}
-//
-//	linuxMipsel32 {
-//	}
-//
-//	linuxX64 {
-//	}
+	linuxArm32Hfp {
+	}
+
+	linuxMips32 {
+	}
+
+	linuxMipsel32 {
+	}
+
+	linuxX64 {
+	}
 
 	macosX64 {
 	}
 
-//	mingwX64 {
-//	}
-//
-//	wasm32 {
-//	}
+	mingwX64 {
+	}
+
+	wasm32 {
+	}
 
 	sourceSets {
 		val commonMain by getting {
-			configureSourceSet(this)
+			configureSourceSet(this, "common")
 
 			dependencies {
 				implementation(kotlin("stdlib-common"))
@@ -72,7 +72,7 @@ kotlin {
 		}
 
 		val commonTest by getting {
-			configureSourceSet(this)
+			configureSourceSet(this, "common")
 
 			dependencies {
 				implementation(kotlin("test-common"))
@@ -81,80 +81,80 @@ kotlin {
 		}
 
 		val nativeMain by creating {
-			configureSourceSet(this)
+			configureSourceSet(this, "native")
 
 			dependsOn(commonMain)
 		}
 		val nativeTest by creating {
-			configureSourceSet(this)
+			configureSourceSet(this, "native")
 
 			dependsOn(commonTest)
 		}
 
-//		val androidNativeArm32Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val androidNativeArm32Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-//
-//		val androidNativeArm64Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val androidNativeArm64Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-//
-//		val iosArm32Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val iosArm32Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-//
-//		val iosArm64Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val iosArm64Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-//
-//		val iosX64Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val iosX64Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
+		val androidNativeArm32Main by getting {
+			configureSourceSet(this, "android_native_arm32")
+
+			dependsOn(nativeMain)
+		}
+		val androidNativeArm32Test by getting {
+			configureSourceSet(this, "android_native_arm32")
+
+			dependsOn(nativeTest)
+		}
+
+		val androidNativeArm64Main by getting {
+			configureSourceSet(this, "android_native_arm64")
+
+			dependsOn(nativeMain)
+		}
+		val androidNativeArm64Test by getting {
+			configureSourceSet(this, "android_native_arm64")
+
+			dependsOn(nativeTest)
+		}
+
+		val iosArm32Main by getting {
+			configureSourceSet(this, "ios_arm32")
+
+			dependsOn(nativeMain)
+		}
+		val iosArm32Test by getting {
+			configureSourceSet(this, "ios_arm32")
+
+			dependsOn(nativeTest)
+		}
+
+		val iosArm64Main by getting {
+			configureSourceSet(this, "ios_arm64")
+
+			dependsOn(nativeMain)
+		}
+		val iosArm64Test by getting {
+			configureSourceSet(this, "ios_arm64")
+
+			dependsOn(nativeTest)
+		}
+
+		val iosX64Main by getting {
+			configureSourceSet(this, "ios_x64")
+
+			dependsOn(nativeMain)
+		}
+		val iosX64Test by getting {
+			configureSourceSet(this, "ios_x64")
+
+			dependsOn(nativeTest)
+		}
 
 		val jsMain by getting {
-			configureSourceSet(this)
+			configureSourceSet(this, "js")
 
 			dependencies {
 				implementation(kotlin("stdlib-js"))
 			}
 		}
 		val jsTest by getting {
-			configureSourceSet(this)
+			configureSourceSet(this, "js")
 
 			dependencies {
 				implementation(kotlin("test-js"))
@@ -162,14 +162,14 @@ kotlin {
 		}
 
 		val jvmMain by getting {
-			configureSourceSet(this)
+			configureSourceSet(this, "jvm")
 
 			dependencies {
 				implementation(kotlin("stdlib-jdk8"))
 			}
 		}
 		val jvmTest by getting {
-			configureSourceSet(this)
+			configureSourceSet(this, "jvm")
 
 			dependencies {
 				implementation(kotlin("test"))
@@ -177,100 +177,102 @@ kotlin {
 			}
 		}
 
-//		val linuxArm32HfpMain by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val linuxArm32HfpTest by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-//
-//		val linuxMips32Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val linuxMips32Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-//
-//		val linuxMipsel32Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val linuxMipsel32Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-//
-//		val linuxX64Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val linuxX64Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-
-		val macosX64Main by getting {
-			configureSourceSet(this)
+		val linuxArm32HfpMain by getting {
+			configureSourceSet(this, "linux_arm32_hfp")
 
 			dependsOn(nativeMain)
 		}
-		val macosX64Test by getting {
-			configureSourceSet(this)
+		val linuxArm32HfpTest by getting {
+			configureSourceSet(this, "linux_arm32_hfp")
 
 			dependsOn(nativeTest)
 		}
 
-//		val mingwX64Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val mingwX64Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
-//
-//		val wasm32Main by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeMain)
-//		}
-//		val wasm32Test by getting {
-//			configureSourceSet(this)
-//
-//			dependsOn(nativeTest)
-//		}
+		val linuxMips32Main by getting {
+			configureSourceSet(this, "linux_mips32")
+
+			dependsOn(nativeMain)
+		}
+		val linuxMips32Test by getting {
+			configureSourceSet(this, "linux_mips32")
+
+			dependsOn(nativeTest)
+		}
+
+		val linuxMipsel32Main by getting {
+			configureSourceSet(this, "linux_mipsel32")
+
+			dependsOn(nativeMain)
+		}
+		val linuxMipsel32Test by getting {
+			configureSourceSet(this, "linux_mipsel32")
+
+			dependsOn(nativeTest)
+		}
+
+		val linuxX64Main by getting {
+			configureSourceSet(this, "linux_x64")
+
+			dependsOn(nativeMain)
+		}
+		val linuxX64Test by getting {
+			configureSourceSet(this, "linux_x64")
+
+			dependsOn(nativeTest)
+		}
+
+		val macosX64Main by getting {
+			configureSourceSet(this, "macos_x64")
+
+			dependsOn(nativeMain)
+		}
+		val macosX64Test by getting {
+			configureSourceSet(this, "macos_x64")
+
+			dependsOn(nativeTest)
+		}
+
+		val mingwX64Main by getting {
+			configureSourceSet(this, "mingw_x64")
+
+			dependsOn(nativeMain)
+		}
+		val mingwX64Test by getting {
+			configureSourceSet(this, "mingw_x64")
+
+			dependsOn(nativeTest)
+		}
+
+		val wasm32Main by getting {
+			configureSourceSet(this, "wasm32")
+
+			dependsOn(nativeMain)
+		}
+		val wasm32Test by getting {
+			configureSourceSet(this, "wasm32")
+
+			dependsOn(nativeTest)
+		}
 	}
 }
 
 /**
  *
  */
-fun configureSourceSet(sourceSet: KotlinSourceSet) {
+fun configureSourceSet(sourceSet: KotlinSourceSet, packageName: String) {
 	val compilation = if (sourceSet.name.endsWith("Main")) "main" else "test"
-	val name = sourceSet.name.replace(compilation.capitalize(), "")
+//	val name = sourceSet.name.replace(compilation.capitalize(), "")
 	val srcDir = "src/$compilation/kotlin"
-	val base = "$projectDir/$srcDir/com/daniloaraujosilva/mathemagika/common/$name"
+	val base = "$projectDir/$srcDir/com/daniloaraujosilva/mathemagika/common/$packageName"
 
-	sourceSet.kotlin.setSrcDirs(listOf(srcDir))
-	sourceSet.kotlin.exclude {
-		val absolutePath = it.file.absolutePath
+	sourceSet.kotlin.setSrcDirs(listOf(base))
 
-		if (base.startsWith(absolutePath)) false else !absolutePath.startsWith(base)
-	}
+//	sourceSet.kotlin.setSrcDirs(listOf(srcDir))
+//	sourceSet.kotlin.exclude {
+//		val absolutePath = it.file.absolutePath
+//
+//		if (base.startsWith(absolutePath)) false else !absolutePath.startsWith(base)
+//	}
 
 	sourceSet.resources.setSrcDirs(listOf("src/$compilation/resources/$name"))
 }
--- library/src/main/kotlin/com/daniloaraujosilva/mathemagika/library/macos_x64/Executor.kt
+++ library/src/main/kotlin/com/daniloaraujosilva/mathemagika/library/macos_x64/Executor.kt
@@ -1,5 +1,7 @@
+//package com.daniloaraujosilva.mathemagika.library.common
+//
 //actual fun execute(command: String): String {
 //	platform.posix.system("git diff > output.txt")
 //
-//	TODO()
+//	return ""
 //}

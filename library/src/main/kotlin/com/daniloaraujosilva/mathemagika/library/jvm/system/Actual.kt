7afd79714ca1ba99792a945e891450195f7bc485
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-06-14 19:47:03 -0300

    Adding actual implementations for the jvm module.

---
+++ library/src/main/kotlin/com/daniloaraujosilva/mathemagika/library/jvm/system/Actual.kt
@@ -0,0 +1,37 @@
+package com.daniloaraujosilva.mathemagika.library.common.system
+
+import java.lang.RuntimeException
+
+/**
+ *
+ */
+actual fun executeSystemCommand(commands: List<String>): String {
+	val process = Runtime.getRuntime().exec(commands.toTypedArray())
+	val error = process.errorStream.bufferedReader().use { it.readText() }
+
+	if (error.isNotBlank()) {
+		val errorNumber = process.exitValue()
+		throw RuntimeException(
+			"""
+				An error occurred when executing the command "${commands.joinToString(" ")}}".
+				Error code: $errorNumber
+				Error: "$error".
+			""".trimIndent()
+		)
+	}
+
+	return process.inputStream.bufferedReader().use { it.readText() }
+}
+
+/**
+ *
+ */
+actual fun getEnvironmentVariable(candidates: List<String>, default: String?): String? {
+	candidates.forEach {
+		val value = System.getenv(it)
+
+		if (value != null) return value
+	}
+
+	return null
+}

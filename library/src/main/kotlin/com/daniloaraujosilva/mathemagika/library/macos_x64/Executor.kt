a54b45620ffa970acefe0c22febb586c37a861b6
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-06-14 19:45:39 -0300

    Changing macos target.

--- library/src/main/kotlin/com/daniloaraujosilva/mathemagika/library/macos_x64/Executor.kt
+++
@@ -1,7 +0,0 @@
-//package com.daniloaraujosilva.mathemagika.library.common
-//
-//actual fun execute(command: String): String {
-//	platform.posix.system("git diff > output.txt")
-//
-//	return ""
-//}
---
+++ library/src/main/kotlin/com/daniloaraujosilva/mathemagika/library/macos_x64/system/Actual.kt
@@ -0,0 +1,17 @@
+package com.daniloaraujosilva.mathemagika.library.common.system
+
+/**
+ * TODO Verify how to get the error output.
+ */
+actual fun executeSystemCommand(commands: List<String>): String {
+//	val returnCode =  platform.posix.system(commands)
+
+	TODO()
+}
+
+/**
+ *
+ */
+actual fun getEnvironmentVariable(candidates: List<String>, default: String?): String? {
+	TODO()
+}

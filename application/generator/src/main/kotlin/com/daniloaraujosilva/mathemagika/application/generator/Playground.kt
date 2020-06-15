71523ee051b644b5148251aaac5eb7563d4299fd
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-06-14 22:41:50 -0300

    Adding code the the playground.

--- application/generator/src/main/kotlin/com/daniloaraujosilva/mathemagika/application/generator/Playground.kt
+++ application/generator/src/main/kotlin/com/daniloaraujosilva/mathemagika/application/generator/Playground.kt
@@ -1,8 +1,24 @@
 package com.daniloaraujosilva.mathemagika.application.generator
 
-import com.daniloaraujosilva.mathemagika.library.jvm.*
-import com.daniloaraujosilva.mathemagika.library.jvm.functions.*
+import com.daniloaraujosilva.mathemagika.library.common.mathematica.functions.*
+import com.daniloaraujosilva.mathemagika.library.common.mathematica.*
 
+@ExperimentalStdlibApi
 @ExperimentalUnsignedTypes
 fun playground() {
+//	val wolframScriptPath = System.getenv("WOLFRAM_SCRIPT_PATH")
+//	println(wolframScriptPath)
+//	println(executeSystemCommand(listOf(wolframScriptPath, "-code", "f[n_] := f[n] = f[n - 1] + f[n - 2]; f[1] = f[2] = 1; Table[f[n],{n,5}]")))
+
+//	println(run("Prime[10]"))
+//	println(run<Double>(zeta(2) + "//N") + 2)
+//	println(zeta(2).run<Int?>())
+	println(
+		com.daniloaraujosilva.mathemagika.library.jvm.functions.zeta(2).runUsingJLink<String>(
+			mapOf(
+				type to EvaluationTypeEnum.OUTPUT_FORM,
+				"pageWidth" to 100
+			)
+		)
+	)
 }
